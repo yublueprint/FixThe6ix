383462c779d7439978e86364f3a265a1788d07dc
Wendy Shen <wendyshen2005@gmail.com>
2026-03-21 13:47:22 -0400

    fixed validation logic to be simpler and more robust

--- app/api/stores/zodValidation.ts
+++ app/api/stores/zodValidation.ts
@@ -5,7 +5,7 @@ const categoryList = ["FAST_FOOD", "GROCERY", "CLOTHING", "RESTAURANT"];
 const categoryEnum = z.enum(categoryList)
 
 const storeSchema = z.object({
-    name: z.string(),
+    name: z.string().trim().min(1),
     category: categoryEnum,
     logoUrl: z.string().optional()
 
@@ -14,35 +14,25 @@ const storeSchema = z.object({
 // get api validation, both values are optional
 export const getStores = (name: string | null, category: string | null) => {
 
-    // validate schema if both filled in
-    const resultWithBoth = storeSchema.safeParse({name: name, category: category});
-    // validate schema if only name filled in
-    const resultWithName = storeSchema.safeParse({name: name, category: categoryList[0]});
-    // validate schema if only category filled in
-    const resultWithCat = storeSchema.safeParse({name: "", category: category});
-    // validate schema if neither filled in
-    const resultWithoutBoth = storeSchema.safeParse({name: "", category: categoryList[0]});
+    return storeSchema.safeParse({
+    name: name ?? undefined,
+    category: category ?? undefined,
+  });
 
-    // if any of the above validations passed, true
-    if(resultWithBoth.success || resultWithName.success || resultWithCat.success || resultWithoutBoth.success) {
-        return true;
-    }
-
-    return false;
 }
 
 // post api validation
 export const postStore = (name: string, category: string, logoUrl: string | null) => {
-    var result;
     
     // if logoUrl passed
-    if(logoUrl){
-        result = storeSchema.safeParse({name: name, category: category, logoUrl: logoUrl});
-    } else { //if no logoUrl
-        result = storeSchema.safeParse({name: name, category: category});
+    const result = storeSchema.safeParse({
+        name,
+        category,
+        ...(logoUrl ? { logoUrl } : {}),
+    });
 
-    }
-    // if schema good, good :)
+
+    // if schema good
     if(!result.success){
         console.log(result);
         return false;
@@ -50,21 +40,3 @@ export const postStore = (name: string, category: string, logoUrl: string | null
 
     return true;
 }
-
-// expect: true
-// console.log(getStores("name 1", "FAST_FOOD"));
-// // expect: false
-// // expect: false
-// // expect: false
-// // expect: false
-// console.log(getStores(null, "FAST_FOOD"));
-// // expect: false
-// console.log(getStores("name 1", null));
-// // expect: false
-// console.log(getStores(null, null));
-
-// // expect: true
-// console.log(postStore("name 2", "FAST_FOOD", "url 2"));
-// // expect: true
-// console.log(postStore("name 2", "FAST_FOOD", null));
-// // expect: false
